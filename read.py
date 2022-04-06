34eb22eda8271721cdfbf23deab8d3fe046cc10d
Surya <amy.renaissance@gmail.com>
2022-04-06 10:26:39 +0800

    add calcuate lenth of average reviews

--- read.py
+++ read.py
@@ -6,8 +6,12 @@ with open('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0:  # % 用來求餘數 ＃如果 count 是 1000 的倍數才印出來
 			print(len(data))
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------------------')
-print(data[1])
+# 算出每筆資料的平均長度
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)  # sum_len = sum_len + len(d) # 累積留言長度加總
+
+print('留言的平均長度為', sum_len/len(data))
