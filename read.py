23df3084cfd44f4d835cdd5db5e6eb289f31c0b6
Surya <amy.renaissance@gmail.com>
2022-04-06 10:50:08 +0800

    add review filtering

--- read.py
+++ read.py
@@ -8,6 +8,7 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
+
 # 算出每筆資料的平均長度
 
 sum_len = 0
@@ -15,3 +16,12 @@ for d in data:
 	sum_len += len(d)  # sum_len = sum_len + len(d) # 累積留言長度加總
 
 print('留言的平均長度為', sum_len/len(data))
+
+
+# 篩選長度小於 100 的留言
+
+new = []   #建立新清單
+for d in data:
+	if len(d) < 100:
+		new.append(d)  #如果長度小於 100 就把你裝入 new 這個新清單
+print('一共有', len(new), '筆留言長度小於100個字元')
